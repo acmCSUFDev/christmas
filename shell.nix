fb8c97c0a2113de310ea465ef99a704dc1eacaea
diamondburned <diamond@arikawa-hi.me>
2023-11-30 17:09:57 -0800

    Tidy up Nix shell

--- shell.nix
+++ shell.nix
@@ -7,24 +7,6 @@ in
 with pkgs.lib;
 with builtins;
 
-let
-	gogio = pkgs.buildGoModule rec {
-		pname = "gogio";
-		version = "7cb98d05";
-
-		src = pkgs.fetchgit {
-			url = "https://git.sr.ht/~eliasnaur/gio-cmd";
-			rev = version;
-			sha256 = "sha256-sCNmTSBdg5CG2zdydd83OFjffIshtfEAIVLuHBXIckk=";
-		};
-
-		vendorSha256 = "sha256-2LQCFYyEletx+FswLV1Ui506qG62yHUKGr5vP5Y/b/s=";
-
-		doCheck = false;
-		subPackages = [ "gogio" ];
-	};
-in
-
 pkgs.mkShell {
 	buildInputs = with pkgs; [
 		bash
@@ -33,24 +15,14 @@ pkgs.mkShell {
 		moreutils # for parallel
 		ffmpeg-full
 
-		# Protobuf tools.
-		protobuf
-		protolint
-		protoc-gen-go
-
 		# Go tools.
 		go
 		gopls
 		gotools
 		go-tools # staticcheck
-
-		# WebAssembly.
-		tinygo
 	];
 
 	shellHook = ''
 		export PATH="$PATH:${toString ./.}/bin"
 	'';
-
-	CGO_ENABLED = "1";
 }
